e5a7b9c50d07db0c97465df41797c2d552e05a7f
Rostislav Grigoryev <ros@woinc.ru>
2017-05-21 03:05:41 +0300

    Change in case when port mapping null

--- scripts/get_docker_format_run.sh
+++ scripts/get_docker_format_run.sh
@@ -10,25 +10,31 @@ function format_run() {
     envvars=$(     echo $json | jq -r '(.[0].Config.Env | [" -e " + .[]] | join(""))'  )
     image=$(       echo $json | jq -r .[0].Image                                       )
     cmd=$(         echo $json | jq -r '.[0].Config.Cmd | join(" ")'                    )
-    mapped_to_container=`(echo $json| jq '.[0].NetworkSettings .Ports' | jq '.[]' | jq '.[]' | jq '.HostPort' | tr -d '"' | cut -f 1- -d\/ --output-delimiter=$'\n' )`
-    mapped_to_host=$(echo $json | jq -r '.[0].NetworkSettings.Ports | keys | to_entries |  map(.value) | join(" ")')
+    
+    mapped_to_container_temp=`(echo $json| jq '.[0].NetworkSettings .Ports' | jq '.[]'   )`
+    
+    if [[ "$mapped_to_container_temp" != null ]];
+    then
+        mapped_to_container=`(echo $json| jq '.[0].NetworkSettings .Ports' | jq '.[]' | jq '.[]' | jq '.HostPort' | tr -d '"' | cut -f 1- -d\/ --output-delimiter=$'\n' )`
+        mapped_to_host=$(echo $json | jq -r '.[0].NetworkSettings.Ports | keys | to_entries |  map(.value) | join(" ")')
+        array=(${mapped_to_container// / })
+        array2=(${mapped_to_host// / })
 
-    array=(${mapped_to_container// / })
-    array2=(${mapped_to_host// / })
+        ports_result=()
 
-    ports_result=()
-
-    for i in "${!array[@]}"
-    do
-        for b in "${!array2[@]}"
+        for i in "${!array[@]}"
         do
-          if [[ "$i" == "$b" ]];
-          then
-            port_map=`(echo "-p ${array[i]}:${array2[b]}")`
-            ports_result+=($port_map)
-          fi
-         done
-    done
-
-    echo "docker run --entrypoint $entrypoint $envvars ${ports_result[@]} $image $cmd"
+            for b in "${!array2[@]}"
+            do
+              if [[ "$i" == "$b" ]];
+              then
+                port_map=`(echo "-p ${array[i]}:${array2[b]}")`
+                ports_result+=($port_map)
+              fi
+             done
+        done
+        echo "docker run --entrypoint $entrypoint $envvars ${ports_result[@]} $image $cmd"
+    else
+        echo "docker run --entrypoint $entrypoint $envvars ${ports_result[@]} $image $cmd"
+    fi
 }
